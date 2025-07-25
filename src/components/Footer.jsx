517d1272bb6503cad05d8d2c281e5551ac419047
sami <thisissamiali@gmail.com>
2025-07-25 13:02:56 +0000

    fixed link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ export const Footer = ({ name }) => {
   return (
     <div className="flex justify-center items-center m-8">
       <div className="text-gray-500 mr-2 text-normal">Made with ❤️ by</div>
-      <div className="underline text-gray-500 text-lg font-bold">{name}</div>
+      <a
+        href="https://www.linkedin.com/in/samiali26/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline text-gray-500 text-lg font-bold"
+      >
+        {name}
+      </a>
     </div>
   );
 };
